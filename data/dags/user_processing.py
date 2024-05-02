d0b1d8e710bb18e1c2f21a64d17078b20f1a8520
Jatinjangid104 <96719935+Jatinjangid104@users.noreply.github.com>
2024-05-03 02:12:39 +0530

    user_processing.pyasf

--- data/dags/user_processing.py
+++ data/dags/user_processing.py
@@ -8,4 +8,15 @@ with DAG(
     schedule_interval="@daily",
     catchup=False
 ) as dag:
-    None
+
+    CreateTable = PostgresOperator(
+        task_id = 'CreateTable',
+        postgre_conn_id = 'postrges',
+        sql = '''CREATE TABLE IF NOT EXISTS users (
+        firstname TEXT NOT NULL,
+        lastname TEXT NOT NULL,
+        country TEXT NOT NULL,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        email TEXT NOT NULL ); '''
+        )
