d543da3d8c63f10f66508572e264b0ed692c1441
Jatinjangid104 <jatin.jangid.104@gmail.com>
2024-05-06 14:01:44 +0530

    pro

--- data/dags/user_processing.py
+++ data/dags/user_processing.py
@@ -1,7 +1,27 @@
 from airflow import DAG
 from datetime import datetime
-from airflow.providers.postgres.operators.postgres import PostgresOperator
 
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.python import PythonOperator
+
+import json
+from pandas import json_normalize # used in _process_user
+
+# The python function to call
+def _process_user(ti):
+    user = ti.xcom_pull(task_ids="extract_user") # fetch data pushed by the previous task extract_user
+    user = user['results'][0]
+    processed_user = json_normalize({
+        'firstname': user['name']['first'],
+        'lastname': user['name']['last'],
+        'country': user['location']['country'],
+        'username': user['login']['username'],
+        'password': user['login']['password'],
+        'email': user['email'] })
+    processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)  
+    
 with DAG(
     dag_id="user_processing",
     start_date=datetime(2023, 1, 1),
@@ -9,14 +29,40 @@ with DAG(
     catchup=False
 ) as dag:
     
-    CreateTable = PostgresOperator(
-        task_id = 'CreateTable',
-        postgre_conn_id = 'postrges',
-        sql = '''CREATE TABLE IF NOT EXISTS users (
-        firstname TEXT NOT NULL,
-        lastname TEXT NOT NULL,
-        country TEXT NOT NULL,
-        username TEXT NOT NULL,
-        password TEXT NOT NULL,
-        email TEXT NOT NULL ); '''
+    create_table = PostgresOperator(
+        task_id='create_table',
+        postgres_conn_id='postgres',
+        sql='''
+            CREATE TABLE IF NOT EXISTS users (
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                country TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL
+            );
+        ''')
+        
+
+    is_api_available = HttpSensor(
+        task_id='is_api_available',
+        http_conn_id='user_api',
+        endpoint='api/'
+    )
+    
+    extract_user = SimpleHttpOperator(
+        task_id='extract_user',
+        http_conn_id='user_api',
+        endpoint='api/',
+        method='GET',
+        response_filter=lambda response: json.loads(response.text),
+        log_response=True
+    )
+    
+    process_user = PostgresOperator(
+        task_id = 'process_user',
+        python_callable=_process_user,
+        
         )
+    
+    extract_user >> process_user
